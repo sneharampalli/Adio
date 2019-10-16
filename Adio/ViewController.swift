1f4b56a5aa73eab96fff07f6f27e245a05d0edac
Sneha Rampalli <svrampalli@gmail.com>
2019-10-15 20:13:36 -0400

    First commit: added playing and pausing an audio file functionality

--- Adio/ViewController.swift
+++ Adio/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+        
+    @IBOutlet weak var playBTN: UIButton!
+    var soundPlayer : AVAudioPlayer!
+        
+    func setupPlayer() {
+        let audioFilename = Bundle.main.path(forResource: "BeautifulNow", ofType: "m4a")
+        do {
+            soundPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioFilename!))
+            soundPlayer.delegate = self
+            soundPlayer.prepareToPlay()
+            soundPlayer.volume = 1.0
+        } catch {
+            print(error)
+        }
+    }
+    
+    @IBAction func pauseAudio(_ sender: UIButton) {
+        if playBTN.titleLabel?.text == "Play" {
+            playBTN.setTitle("Stop", for: .normal)
+            setupPlayer()
+            soundPlayer.play()
+        } else {
+            soundPlayer.stop()
+            playBTN.setTitle("Play", for: .normal)
+        }
+    }
 }
 
