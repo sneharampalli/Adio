7a4331d1b1db48d66814eacf408092490b99327b
Romit Nagda <32246080+rnagda@users.noreply.github.com>
2020-01-21 02:11:02 -0500

    Fixed Scene Delegate bug

--- Adio/SceneDelegate.swift
+++ Adio/SceneDelegate.swift
@@ -39,29 +39,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate, SPTAppRemoteDelegate {
     }()
 
     var window: UIWindow?
-
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
-    }
-    
-    lazy var spotifyConfiguration = SPTConfiguration(
-        clientID: spotifyClientID,
-        redirectURL: spotifyRedirectURL
-    )
-    
-    lazy var appRemote: SPTAppRemote = {
-        let appRemote = SPTAppRemote(configuration: self.spotifyConfiguration, logLevel: .debug)
-        appRemote.connectionParameters.accessToken = self.accessToken
-        appRemote.delegate = self
-        //appRemote.playerAPI?.delegate = self
-        return appRemote
-    }()
-
-    var window: UIWindow?
     
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
         guard let url = URLContexts.first?.url else {
