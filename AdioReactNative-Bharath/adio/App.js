f7ff6ac53f5d30dd3f774be2ff56e4c54840f850
Bharath Jaladi <bharathjaladi98@gmail.com>
2020-02-11 23:50:57 -0500

    Updated login flow, some styling

--- AdioReactNative-Bharath/adio/App.js
+++ AdioReactNative-Bharath/adio/App.js
@@ -2,10 +2,31 @@ import React from 'react';
 import Root from './src/Root';
 import Amplify from 'aws-amplify';
 import awsmobile from './src/aws-exports';
-import { withAuthenticator } from 'aws-amplify-react-native';
+import { withAuthenticator, AmplifyTheme } from 'aws-amplify-react-native';
 
 Amplify.configure(awsmobile);
-const AppWithAuth = withAuthenticator(Root, true);
+// const MyTheme = {
+//   button: { backgroundColor: "green", borderColor: "red" },
+//   signInButtonIcon: { display: "none" }
+// };
+const signUpConfig = {
+  hiddenDefaults: ["username"],
+  signUpFields: [
+    { label: "First", key: "name", required: true, type: "string", displayOrder: 1 },
+    { label: "Last", key: "family_name", required: true, type: "string", displayOrder: 2 },
+    { label: "Email", key: "email", required: true, type: "string", displayOrder: 3 },
+    { label: "Phone Number", key: "phone_number", required: true, type: "string", displayOrder: 4 },
+    { label: "Password", key: "password", required: true, type: "string", displayOrder: 5 }
+  ]
+};
+const usernameAttributes = 'Email';
+const MySectionHeader = Object.assign({}, AmplifyTheme.container, { backgroundColor: '#96D1C7' });
+const MyTheme = Object.assign({}, AmplifyTheme, { container: MySectionHeader });
+const AppWithAuth = withAuthenticator(Root, {
+  signUpConfig, usernameAttributes
+}, [], null, MyTheme);
+
+
 
 export default class App extends React.Component {
   render() {
--- AdioReactNative-Bharath/adio/src/Root.js
+++ AdioReactNative-Bharath/adio/src/Root.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Storage } from 'aws-amplify';
+import { Storage, Auth } from 'aws-amplify';
 import { Audio } from "expo-av";
-import { StyleSheet, Text, View, FlatList, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TextInput } from 'react-native';
 import { API, graphqlOperation } from 'aws-amplify';
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import * as queries from "./graphql/queries";
 import * as mutations from "./graphql/mutations";
 import * as subscriptions from "./graphql/subscriptions";
@@ -13,83 +15,61 @@ export default class Root extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            quotes: [],
-            input: ""
+            sessionActive: false,
         }
     }
 
     componentDidMount() {
         this.getAdsList();
-        // API.graphql(graphqlOperation(subscriptions.onCreateAd)).subscribe({
-        //     next: (data) => {
-        //         console.log(data);
-        //         const addedQuote = data.value.data.onCreateQuote;
-        //         this.setState({ quotes: [addedQuote, ...this.state.quotes] });
-        //     }
-        // });
-        // API.graphql(graphqlOperation(subscriptions.onDeleteQuote)).subscribe({
-        //     next: (data) => {
-        //         console.log(data);
-        //         const removedQuote = data.value.data.onDeleteQuote;
-        //         const updatedList = this.state.quotes.filter((quote => {
-        //             return quote.id !== removedQuote.id;
-        //         }))
-        //         this.setState({ quotes: updatedList });
-        //     }
-        // });
+    }
+
+    signOut = () => {
+        Auth.signOut()
+            .then(() => {
+                this.props.onStateChange('signedOut', null);
+            })
+            .catch(err => {
+                console.log('err: ', err)
+            });
     }
 
     getAdsList = async () => {
+        const latLongObj = {
+            maxLat: 40,
+            minLat: 39.8,
+            maxLong: -75,
+            minLong: -75.2
+        }
         try {
-            const latLongObj = {
-                maxLat: 40,
-                minLat: 39.8,
-                maxLong: -75,
-                minLong: -75.2
-            }
             const response = await API.graphql(graphqlOperation(queries.listAds, { input: latLongObj }));
             this.setState({ ads: response.data.listAds.items });
             const url = await Storage.get('BeautifulNow.m4a', { customPrefix: { public: '', protected: '', private: '' } });
-            //console.log(url);
-            //const publicUrl = "https://s3.amazonaws.com/adioc492d9a3e7204c369b78b9a304571a10215215-adio/BeautifulNow.m4a";
-            await Audio.setAudioModeAsync({ playsInSilentModeIOS: true, interruptionModeIOS: INTERRUPTION_MODE_IOS_DUCK_OTHERS, staysActiveInBackground: true });
+            const publicUrl = "https://s3.amazonaws.com/adioc492d9a3e7204c369b78b9a304571a10215215-adio/BeautifulNow.m4a";
+            await Audio.setAudioModeAsync({ playsInSilentModeIOS: true, interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DUCK_OTHERS, staysActiveInBackground: true, interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DUCK_OTHERS });
             const soundObject = new Audio.Sound();
-            try {
-                await soundObject.loadAsync({ uri: url });
-                await soundObject.playAsync();
-                // Your sound is playing!
-            } catch (error) {
-                // An error occurred!
-            }
-
+            await soundObject.loadAsync({ uri: url });
+            await soundObject.playAsync();
         } catch (err) {
             console.error(err);
         }
     }
 
-    addPost = async () => {
-        try {
-            const postObj = {
-                uniqueID: "doug@walmart.com-10:02",
-                campaignName: "walmartC",
-                adName: "make money quick",
-                owner: "doug@walmart.com",
-                maxLat: 40,
-                minLat: 39.8,
-                maxLng: -75,
-                minLng: -75.2,
-                file: { bucket: "adioc492d9a3e7204c369b78b9a304571a10215215-adio", region: "us-east-1", key: "BeautifulNow.m4a" }
-
-            }
-            await API.graphql(graphqlOperation(mutations.createAd, { input: postObj }))
-            this.setState({ input: "" });
-        } catch (err) {
-            console.error(err);
-        }
-    }
-    // deleteQuote = async (id) => {
+    // addPost = async () => {
     //     try {
-    //         await API.graphql(graphqlOperation(mutations.deleteQuote, { input: { id: id } }))
+    //         const postObj = {
+    //             uniqueID: "doug@walmart.com-10:02",
+    //             campaignName: "walmartC",
+    //             adName: "make money quick",
+    //             owner: "doug@walmart.com",
+    //             maxLat: 40,
+    //             minLat: 39.8,
+    //             maxLng: -75,
+    //             minLng: -75.2,
+    //             file: { bucket: "adioc492d9a3e7204c369b78b9a304571a10215215-adio", region: "us-east-1", key: "BeautifulNow.m4a" }
+
+    //         }
+    //         await API.graphql(graphqlOperation(mutations.createAd, { input: postObj }))
+    //         this.setState({ input: "" });
     //     } catch (err) {
     //         console.error(err);
     //     }
@@ -98,20 +78,33 @@ export default class Root extends React.Component {
     render() {
 
         return (
-            <View style={styles.container}>
-                <TextInput
+            <View style={styles.container, { flex: 1, backgroundColor: '#96D1C7' }
+            }>
+                {/* <TextInput
                     style={{ height: 40 }}
                     value={this.state.input}
                     placeholder="Type a post!"
                     onChangeText={(text) => this.setState({ input: text })}
                 />
-                <Button title="Add Post" onPress={this.addPost} />
+                <Button title="Add Post" onPress={this.addPost} /> */}
                 <FlatList
                     data={this.state.ads}
                     renderItem={({ item }) => (
                         <View key={item.id}>
-                            <Text style={styles.item}>"{item.adName}"</Text>
-                            <Text style={styles.item}>- {item.file.key}</Text>
+                            <Button
+                                onPress={this.signOut}
+                                //() => { this.setState({ sessionActive: !this.state.sessionActive }); }}
+                                type="clear"
+                                icon={
+                                    <Icon
+                                        name={this.state.sessionActive ? "pause-circle" : "play-circle"}
+                                        size={200}
+                                        color="white"
+                                    />
+                                }
+                            />
+                            {!this.state.sessionActive ? <Text style={styles.item}>Active Session</Text> : <Text style={styles.item}>No Active Session</Text>}
+                            {/* <Text style={styles.item}>- {item.file.key}</Text> */}
                             {/* <View style={{ width: 150 }}>
                                 <Button title="Delete Quote" color="#ffa500" onPress={() => this.deleteQuote(item.id)} />
                             </View> */}
@@ -119,7 +112,7 @@ export default class Root extends React.Component {
 
                     )}
                 />
-            </View>
+            </View >
         )
 
     }
