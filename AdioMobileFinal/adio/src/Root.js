640ed0caf86afe6cedf822af87a88cf49745185c
Sneha Advani <sneha.d.advani@gmail.com>
2020-04-23 20:23:58 -0400

    Mobile app code updated for final version

--- AdioMobileFinal/adio/src/Root.js
+++ AdioMobileFinal/adio/src/Root.js
@@ -154,7 +154,7 @@ export default class Root extends React.Component {
         }
         if (!this.state.sessionActive) {
             this.setState({ sessionActive: true });
-            timer.setInterval("ads", this.playAd, this.state.adInterval * 1000);
+            timer.setInterval("ads", this.playAd, this.state.adInterval * 60000);
         } else {
             this.setState({sessionActive: false});
             if (this.state.isPlaying) {
@@ -241,8 +241,7 @@ export default class Root extends React.Component {
         }
         
         let location = await Location.getCurrentPositionAsync({});
-        this.setState({ currLat: 39.952586, currLong: -75.165219 }); // TODO: REPLACE WITH BELOW LINE
-        // this.setState({ currLat: location.coords.latitude, currLong: location.coords.longitude });
+        this.setState({ currLat: location.coords.latitude, currLong: location.coords.longitude });
         console.log("Current location is " + this.state.currLat + " " + this.state.currLong);
     };
 
